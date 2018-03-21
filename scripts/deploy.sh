4418fc6da664c75e578192fa2797350560182df1
Pieter de Bruin <pdebruin@hotmail.com>
2018-03-21 10:01:59 +0100

    scripted

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 #docker stack deploy movieplex -c docker-compose.yml
-docker service update --image 52.232.26.254/dev/movieplex7-client:latest movies-dev_react-client
+docker service update --image 52.232.26.254/dev/movieplex7-client:latest $1
